9ff33a6435b9bebfb5babc30fa80ee302ac923f3
DevDaniloFerrari <consultor.ferrari@leomadeiras.com.br>
2023-03-09 08:48:13 -0300

    create sum method

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿Console.WriteLine("First value: ");
-float firstValue = float.Parse(Console.ReadLine());
+﻿void Sum()
+{
+    Console.WriteLine("First value: ");
+    float firstValue = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Second value: ");
-float secondValue = float.Parse(Console.ReadLine());
+    Console.WriteLine("Second value: ");
+    float secondValue = float.Parse(Console.ReadLine());
 
-Console.WriteLine("");
+    Console.WriteLine("");
 
-Console.WriteLine("The result of sum is: " + (firstValue + secondValue));
+    Console.WriteLine("The result of sum is: " + (firstValue + secondValue));
+}
+
+Sum();
